cffd979f814fa9a430f9fab2beca3ac9d3b2bee2
BrannanGeshwind <brannan.geshwind@gmail.com>
2023-08-23 23:07:17 -0500

    Pushing what I have completed to repo for submission. Need to finish Dynamic API for date range, will resubmit once completed.

--- README.md
+++ README.md
@@ -3,11 +3,19 @@ Repo for Module 10 challenge
 
 ## Updates
 8/23 -Finished analysis code and work in notebook, updated README. Pushing to repo for update.
-
+     -Pushing to repo with project complete up to API for range between two dates. Will resubmit once completed.
+     -Commented code for range section out.
+     
 8/21 -Pushed to repo to add resources and starter code.
 
-
-
-## References
+## Languages and References
 - [SQLAlchemy](https://www.sqlalchemy.org/)
+- [Flask](https://flask.palletsprojects.com/en/2.3.x/)
+- [SQLite](https://www.sqlite.org/docs.html)
+
 - [StackOverflow](https://stackoverflow.com/)
+
+
+### Code Used
+- Flask code used for line 14, starting from "connect_args" from [here](https://stackoverflow.com/questions/15140554/programmingerror-thread-error-in-sqlalchemy)
+- Flask code referenced in line 120 from [here]https://stackoverflow.com/questions/71724579/row-is-not-json-serializable-error-when-sending-result-set-to-a-flask-view)
--- app.py
+++ app.py
@@ -1,30 +1,150 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False}, echo=True)
 
 # reflect an existing database into a new model
+Base = automap_base()
+
 
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """API Routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"        
+        f"/api/v1.0/tobs<br/>"
+        f"<br/>"
+        f"For the following, please input a date where indicated:<br/>"       
+        f"/api/v1.0/startdate<br/>"
+        f"/api/v1.0/startdate/enddate<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Precipitation Information"""
+    
+    # Date Info
+    most_recent_date = session.query(func.max(measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    year_length = most_recent_date - dt.timedelta(days=365)
+    
+    # 12 months of precipitation data
+    precipitation = session.query(measurement.date, measurement.prcp)\
+        .filter(measurement.date >= year_length)\
+        .all()
+    
+    # Convert the query results to a dictionary
+    precipitation_dict = {date: prcp for date, prcp in precipitation}
+
+    # Return precipitation info
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Names of the stations."""
+    
+    # Query and return a JSON list of stations
+    stations = session.query(station.name)
+    station_names = [name[0] for name in stations]
+
+    return jsonify(station_names)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Temperature Observations (tobs) for previous year."""
+    
+    # Date Info
+    most_recent_date = session.query(func.max(measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    year_length = most_recent_date - dt.timedelta(days=365)
+    
+    # Query tobs for most active station
+    most_active_station = session.query(measurement.station, func.count(measurement.station))\
+        .group_by(measurement.station)\
+        .order_by(func.count(measurement.station).desc())\
+        .first()
+    
+    most_active_station_id = most_active_station[0]
+    
+    tobs = session.query(measurement.date, measurement.tobs)\
+        .filter(measurement.station == most_active_station_id)\
+        .filter(measurement.date >= year_length)\
+        .all()
+    
+    # Format the data into dictionary
+    tobs_list = [{"date": date, "temperature": temp} for date, temp in tobs]
+    
+    # Return list of tobs
+    return jsonify(tobs_list)
+
+@app.route("/api/v1.0/<start>")
+def temp_stats_start(start):
+    """Temperature information for a specific date in YYYY-MM-DD format."""
+    
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    
+    # Query and calculate temperature statistics (TMIN, TAVG, TMAX) for dates greater than or equal to start date
+    single_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs))\
+        .filter(measurement.date >= start_date)\
+        .first()
+    
+    single_result = [tuple(single_temp) for row in single_temp]
+    
+    # Return list of temperature statistics
+    return jsonify(single_result[0])
+
+@app.route("/api/v1.0/<start>/<end>")
+"""def temp_stats_range(start, end):
+    ""Temperature information for a specific range in YYYY-MM-DD format.""
+    
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+
+    # Query and calculate temperature statistics (TMIN, TAVG, TMAX) for dates within the start-end range
+    range_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs))\
+        .filter(measurement.date >= start_date)\
+        .filter(measurement.date <= end_date)\
+        .first()    
+    
+    
+    range_results = []
+     
+    # Return list of temperature statistics
+    return jsonify(range_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
